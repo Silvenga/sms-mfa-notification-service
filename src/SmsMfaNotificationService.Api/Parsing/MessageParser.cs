c8fff8269e6bb24bd128d7a10b4d75ff96342380
Mark Lopez <m@silvenga.com>
2023-04-21 20:04:04 -0500

    Improved parsing.

--- src/SmsMfaNotificationService.Api/Parsing/MessageParser.cs
+++ src/SmsMfaNotificationService.Api/Parsing/MessageParser.cs
@@ -9,14 +9,24 @@ namespace SmsMfaNotificationService.Api.Parsing
         private static readonly Regex GenericPattern = new(@"\b(?<code>\d{4,8})\b", RegexOptions.Compiled);
         private static readonly Regex SeparatedPattern = new(@"\b(?<code>\d{3,4}[ -]+\d{3,4})\b", RegexOptions.Compiled);
 
-        private static readonly Regex ZipCodePattern = new(@"(AL|NE|AK|NV|AZ|NH|AR|NJ|CA|NM|CO|NY|CT|NC|DE|ND|DC|OH|FL|OK|GA|OR|HI|PA|ID|PR|IL|RI|IN|SC|IA|SD|KS|TN|KY|TX|LA|UT|ME|VT|MD|VA|MA|VI|MI|WA|MN|WV|MS|WI|MO|WY|MT)[ ,]+\b\d{5}\b", RegexOptions.Compiled);
+        private static readonly Regex ZipCodePattern =
+            new(
+                @"(AL|NE|AK|NV|AZ|NH|AR|NJ|CA|NM|CO|NY|CT|NC|DE|ND|DC|OH|FL|OK|GA|OR|HI|PA|ID|PR|IL|RI|IN|SC|IA|SD|KS|TN|KY|TX|LA|UT|ME|VT|MD|VA|MA|VI|MI|WA|MN|WV|MS|WI|MO|WY|MT)[ ,]+\b\d{5}\b",
+                RegexOptions.Compiled);
+
+        private static readonly Regex RoadPattern = new(@"\b\d{3,4} [a-z]", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        private static readonly Regex CreditCardPattern = new(@"ending (in )?\d{4}\b", RegexOptions.Compiled);
+        private static readonly Regex PhoneNumberPattern = new(@"(\d{3}-){2}\d{4}", RegexOptions.Compiled);
 
         public static bool TryGetCode(string input, [NotNullWhen(true)] out string? code)
         {
             code = default;
             return !ZipCodePattern.IsMatch(input)
-                && (TryRegexMatch(GenericPattern, input, out code)
-                || TryRegexMatch(SeparatedPattern, input, out code));
+                   && !RoadPattern.IsMatch(input)
+                   && !CreditCardPattern.IsMatch(input)
+                   && !PhoneNumberPattern.IsMatch(input)
+                   && (TryRegexMatch(GenericPattern, input, out code)
+                       || TryRegexMatch(SeparatedPattern, input, out code));
         }
 
         private static bool TryRegexMatch(Regex regex, string input, [NotNullWhen(true)] out string? code)
--- tests/SmsMfaNotificationService.Api.Tests/Parsing/MessageParserFacts.cs
+++ tests/SmsMfaNotificationService.Api.Tests/Parsing/MessageParserFacts.cs
@@ -9,10 +9,15 @@ namespace SmsMfaNotificationService.Api.Tests.Parsing
     {
         [Theory]
         [InlineData("Your Discord security code is: 000 000", "000000")]
-        [InlineData("ALERT! DO NOT share this code with anyone. We will never ask you for this code. Verification Code: \r\n\r\n000000 (expires in 3 minutes)", "000000")]
+        [InlineData("ALERT! DO NOT share this code with anyone. We will never ask you for this code. Verification Code: \r\n\r\n000000 (expires in 3 minutes)",
+            "000000")]
         [InlineData("Your Betterment auth code is 000000. If you did not make this request, please email support@betterment.com", "000000")]
-        [InlineData("Your T-Mobile ID verification code is 000000. For your security never share your verification code. T-Mobile will never contact you to ask for your code.", "000000")]
-        [InlineData("CIT Bank Messages. The one-time code is 00000. Please enter and submit it online. Msg&Data rates may apply/STOP if unexpected/HELP for support", "00000")]
+        [InlineData(
+            "Your T-Mobile ID verification code is 000000. For your security never share your verification code. T-Mobile will never contact you to ask for your code.",
+            "000000")]
+        [InlineData(
+            "CIT Bank Messages. The one-time code is 00000. Please enter and submit it online. Msg&Data rates may apply/STOP if unexpected/HELP for support",
+            "00000")]
         [InlineData("G-000000 is your Google verification code.", "000000")]
         [InlineData("PayPal: Your security code is: 000000. It expires in 10 minutes. Don't share this code with anyone.", "000000")]
         [InlineData("From: SoFi\nReminder: SoFi will never ask for your code on a call not initiated by you\nOne-Time Code: 000000", "000000")]
@@ -35,8 +40,31 @@ namespace SmsMfaNotificationService.Api.Tests.Parsing
 
         [Theory]
         [InlineData("Example2 CA 11111")]
-        [InlineData("Example2 CA, 11111")]
+        [InlineData("at place, 6850 N Road.")]
         public void Given_message_containing_an_address_then_return_false(string input)
+        {
+            // Act
+            var result = MessageParser.TryGetCode(input, out _);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData("We declined $000.00 with card ending 0000 at")]
+        [InlineData("000-000-0000")]
+        public void Given_message_containing_a_credit_card_then_return_false(string input)
+        {
+            // Act
+            var result = MessageParser.TryGetCode(input, out _);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData("000-000-0000")]
+        public void Given_message_containing_a_phone_number_then_return_false(string input)
         {
             // Act
             var result = MessageParser.TryGetCode(input, out _);
